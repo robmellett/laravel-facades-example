40d9ca3bdc8f50f4c9473f3eeec84612e36097a8
Rob Mellett <robmellett@gmail.com>
2020-02-20 14:12:38 +1000

    Clean up

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,6 @@
 |
 */
 
-use Contentful\Delivery\Client as DeliveryClient;
-
 Route::get('/', function () {
-
-    $client = \App::make("contentful");
-    $entries = $client->getEntries();
-
-    // $access_token = config('contentful')['delivery.token'];
-    // $space_id = config("contentful")['delivery.space'];
-    // $environment = config("contentful")['delivery.environment'];
-
-    // $client = new DeliveryClient($access_token, $space_id, $environment);
-
-    dd($entries);
-
-    // return view('welcome');
+    return view('welcome');
 });
